a5d79b81674fd06536ff95f2a9785cfbdab98d40
julian.patterson <julian.patterson@gmail.com>
2020-12-30 14:31:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,4 +6,7 @@ sum = first_number + second_number
 difference = first_number - second_number
 
 product = first_number * second_number
+
+quotient = first_number / second_number
+
 # Add your variables here
